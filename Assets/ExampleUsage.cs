384c23e4b6d2c2202c4db4a903718a56995226d5
aleyip <10003371+aleyip@users.noreply.github.com>
2021-11-04 18:02:43 -0300

    Teste com Desktop e Windows Explorer
    https://github.com/Phylliida/UnityWindowsCapture

---
+++ Assets/ExampleUsage.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WinCapture;
+
+public class ExampleUsage : MonoBehaviour {
+
+    Shader windowShader;
+    Shader desktopShader;
+    Shader chromiumShader;
+    WindowCaptureManager captureManager;
+
+    public Dictionary<IntPtr, WindowCapture> windowsRendering;
+    public Dictionary<IntPtr, GameObject> windowObjects;
+
+    DesktopCapture desktopCapture1;
+    GameObject desktopObject;
+
+    ChromiumCapture chromiumCapture;
+    GameObject chromiumObject;
+
+    // Use this for initialization
+    void Start () {
+
+        windowShader = Shader.Find("WinCapture/WindowShader");
+        desktopShader = Shader.Find("WinCapture/DesktopShader");
+        chromiumShader = Shader.Find("WinCapture/ChromiumShader");
+
+        windowsRendering = new Dictionary<IntPtr, WindowCapture>();
+        windowObjects = new Dictionary<IntPtr, GameObject>();
+        captureManager = new WindowCaptureManager();
+        captureManager.OnAddWindow += OnAddWindow;
+        captureManager.OnRemoveWindow += OnRemoveWindow;
+        lastUpdateTime = Time.time;
+        lastPollWindowsTime = Time.time;
+
+
+        int displayNum = 0;
+        desktopCapture1 = new DesktopCapture(displayNum);
+
+        desktopObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        desktopObject.name = "desktop" + displayNum;
+        desktopObject.transform.GetComponent<Renderer>().material = new Material(desktopShader);
+        desktopObject.transform.localEulerAngles = new Vector3(90, 0, 0);
+        return;
+
+        chromiumCapture = new ChromiumCapture(1024, 1024, "http://google.com");
+
+        chromiumObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        chromiumObject.name = "chromium capture";
+        chromiumObject.transform.GetComponent<Renderer>().material = new Material(chromiumShader);
+        chromiumObject.transform.localEulerAngles = new Vector3(90, 0, 0);
+    }
+
+    // You need to do this because the desktop capture API will only work if we are on the graphics thread
+    void OnPostRender()
+    {
+        desktopCapture1.OnPostRender();
+    }
+
+
+    bool IsGoodWindow(WindowCapture window)
+    {
+        Debug.Log("Saw window: " + window.windowInfo.title);
+        // You can stick whatever logic or names you want here for windows you want to keep to render
+
+        string windowLowerTitle = window.windowInfo.title.ToLower();
+        if (windowLowerTitle.Contains("meet"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void OnAddWindow(WindowCapture window)
+    {
+        if (!windowsRendering.ContainsKey(window.hwnd) && IsGoodWindow(window))
+        {
+            GameObject windowObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            windowObject.name = window.windowInfo.title;
+            windowObject.transform.GetComponent<Renderer>().material = new Material(windowShader);
+            windowObject.transform.localEulerAngles = new Vector3(90, 0, 0);
+            windowsRendering[window.hwnd] = window;
+            windowObjects[window.hwnd] = windowObject;
+        }
+    }
+
+    void OnRemoveWindow(WindowCapture window)
+    {
+        Debug.Log("removed " + window.windowInfo.title);
+        if (windowsRendering.ContainsKey(window.hwnd))
+        {
+            GameObject windowObjectRemoving = windowObjects[window.hwnd];
+            Destroy(windowObjectRemoving);
+            windowObjects.Remove(window.hwnd);
+            windowsRendering.Remove(window.hwnd);
+        }
+    }
+
+    float lastUpdateTime;
+    float lastPollWindowsTime;
+
+    public float captureRateFps = 30;
+    public float windowPollPerSecond = 2;
+    public float windowScale = 0.001f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool didChange;
+        //Texture2D chromiumTexture = chromiumCapture.GetChromiumTexture(out didChange);
+        //if (didChange)
+        //{
+        //    chromiumObject.GetComponent<Renderer>().material.mainTexture = chromiumTexture;
+        //}
+        //chromiumObject.transform.localScale = new Vector3(chromiumTexture.width * windowScale, 0.1f, chromiumTexture.height * windowScale);
+
+
+        //if (Time.frameCount == 400)
+        //{
+        //    chromiumCapture.SetUrl("http://reddit.com");
+        //}
+
+        //if (Time.time - lastUpdateTime < 1.0f / captureRateFps)
+        //{
+        //    return;
+        //}
+        //else
+        //{
+        //    lastUpdateTime = Time.time;
+        //}
+
+
+
+        // Capture the desktop
+        Texture2D desktopTexture = desktopCapture1.GetWindowTexture(out didChange);
+
+        desktopObject.transform.localScale = new Vector3(desktopCapture1.desktopWidth * windowScale, 0.1f, desktopCapture1.desktopHeight * windowScale);
+        if (didChange)
+        {
+            desktopObject.transform.GetComponent<Renderer>().material.mainTexture = desktopTexture;
+        }
+
+        // Gets information about position and icon of the cursor so you can render it onto the captured surfaces
+        WindowCapture.UpdateCursorInfo();
+
+        // Capture each window
+        Debug.Log($"Number of windows: {windowsRendering.Count}");
+        foreach (IntPtr key in windowsRendering.Keys)
+        {
+            Debug.Log($"Name of windows: {windowsRendering[key].windowInfo.title}");
+            WindowCapture window = windowsRendering[key];
+            GameObject windowObject = windowObjects[key];
+
+            if (windowObject == null)
+            {
+                continue;
+            }
+
+            Texture2D windowTexture = window.GetWindowTexture(out didChange);
+            if (didChange)
+            {
+                windowObject.GetComponent<Renderer>().material.mainTexture = windowTexture;
+            }
+            windowObject.transform.localScale = new Vector3(window.windowWidth * windowScale, 0.1f, window.windowHeight * windowScale);
+        }
+
+        // Poll for new windows
+        if (Time.time - lastPollWindowsTime < 1.0f / windowPollPerSecond)
+        {
+            return;
+        }
+        else
+        {
+            lastPollWindowsTime = Time.time;
+            // calls OnAddWindow or OnRemoveWindow above if any windows have been added or removed
+            captureManager.Poll();
+        }
+    }
+}
